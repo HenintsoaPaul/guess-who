f0117f1533b1df226ce09f7b5f5374e43a57fe5d
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-12-18 16:48:57 +0300

    api response service

---
+++ app/Services/ApiResponseService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+/**
+ * Service pour gerer les fonctions de dates
+ */
+ class ApiResponseService {
+    public static function apiResponse(int $code , string $message , array $data = [] , array $errors = [] ){
+        $status = ApiResponseService::getResponseStatusByCode($code);
+        $response =[
+            'status'=>$status,
+            'code'=>$code,
+            'message'=>$message,
+            'data'=>$data,
+            'error'=>[
+                'errors' => $errors,
+                'details' => []
+            ],
+        ];
+        return json_encode($response);
+    }
+
+    public static function getResponseStatusByCode($statusCode){
+        switch ($statusCode) {
+            case '200':
+                return 'OK';
+                break;
+            case '400':
+            case '404':
+            case '500':
+                return 'Error';
+                break;
+            default:
+                return 'Undefined';
+                break;
+        }
+    }
+ }
