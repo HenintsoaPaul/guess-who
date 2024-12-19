780ad95b83a9171f50f0ba4dadbbdd4842832f0f
Henintsoa Paul <henintsoapaul@gmail.com>
2024-12-19 06:42:25 +0300

    [migration]-Seed db after migration

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,6 +8,7 @@ use App\Services\JsonResponseService;
 use App\Services\RandomService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
@@ -22,34 +23,37 @@ class LoginController extends Controller
     public function login(Request $request): JsonResponse
     {
         $credentials = null;
+        DB::beginTransaction();
         try {
             $credentials = $request->validate([
                 'email' => 'required|email',
                 'password' => 'required',
             ]);
 
-            DB::transaction(function () use ($credentials) {
-                $account = AuthService::authenticate($credentials['email'], $credentials['password']);
+            $account = AuthService::authenticate($credentials['email'], $credentials['password']);
 
-                // generate pin
-                $pin = RandomService::newPin();
+            // generate pin
+            $pin = RandomService::newPin();
 
-                // send pin on email
-                // TODO: atao eto...
+            // send pin on email
+            // TODO: atao eto...
 
-                // insert pending_auth
-                $pendingAuth = PendingAuth::addNew($pin, $account->id_account);
+            // insert pending_auth
+            $pendingAuth = PendingAuth::addNew($pin, $account->id_account);
 
-                $res = $pendingAuth->save();
-                if (!$res) {
-                    throw new \Exception("Failed to insert pending_auth.");
-                }
-                return $this->jsonResponse->success('Mety ilay controle', $pendingAuth );
-            });
+            $res = $pendingAuth->save();
+            if (!$res) {
+                throw new \Exception("Failed to insert pending_auth.");
+            }
+
+            DB::commit();
+            return $this->jsonResponse->success('Mety ilay controle', $pendingAuth);
+
         } catch (ValidationException $e) {
+            DB::rollBack();
             return $this->jsonResponse->validationError($e);
-
         } catch (\Exception $exception) {
+            DB::rollBack();
             $unauthorized = 401;
             return $this->jsonResponse->error($exception->getMessage(), $credentials, $unauthorized);
         }
