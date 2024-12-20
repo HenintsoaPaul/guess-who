f7b44664586d6970e65a242eb23594f7c2d53983
Henintsoa Paul <henintsoapaul@gmail.com>
2024-12-20 06:38:29 +0300

    [docker]-Add seed comand in Dockerfile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             TypeAccountStateSeeder::class,
+            PendingRegisterSeeder::class,
+            AccountSeeder::class,
         ]);
     }
 }
