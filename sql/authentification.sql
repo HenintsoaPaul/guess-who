223c1d3ce17e0d05b0a7abde1391a030914739ae
Henintsoa Paul <henintsoapaul@gmail.com>
2024-12-20 09:58:45 +0300

    [bdd]-Fix schema

--- sql/authentification.sql
+++ sql/authentification.sql
@@ -15,12 +15,6 @@ CREATE TABLE type_account_state(
    PRIMARY KEY(id_type_account_state)
 );
 
-CREATE TABLE type_pending_auth(
-   id_type_pending_auth SERIAL,
-   val VARCHAR(50)  NOT NULL,
-   PRIMARY KEY(id_type_pending_auth)
-);
-
 CREATE TABLE account(
    id_account SERIAL,
    email VARCHAR(50)  NOT NULL,
@@ -59,10 +53,8 @@ CREATE TABLE pending_auth(
    date_expiration TIMESTAMP NOT NULL,
    date_creation TIMESTAMP NOT NULL,
    pin VARCHAR(50)  NOT NULL,
-   id_type_pending_auth INTEGER NOT NULL,
    id_account INTEGER NOT NULL,
    PRIMARY KEY(id_pending_auth),
-   FOREIGN KEY(id_type_pending_auth) REFERENCES type_pending_auth(id_type_pending_auth),
    FOREIGN KEY(id_account) REFERENCES account(id_account)
 );
 
