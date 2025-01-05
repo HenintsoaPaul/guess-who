4918af15d6e646b8fec5e2de937008a169fb8599
Henintsoa Paul <henintsoapaul@gmail.com>
2025-01-05 09:17:50 +0300

    [app][security]-Allow excluded paths

--- crypto/src/main/java/itu/crypto/CryptoConfigProperties.java
+++ crypto/src/main/java/itu/crypto/CryptoConfigProperties.java
@@ -4,9 +4,14 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Data
 @Component
 public class CryptoConfigProperties {
-    @Value( "${app.laravel-url}")
+    @Value("${app.laravel-url}")
     private String laravelUrl;
+
+    @Value("${app.excluded-paths}")
+    private final List<String> excludedPaths;
 }
