455401ed2a83e603b78c76fb50a5977cd336a9c1
Henintsoa Paul <henintsoapaul@gmail.com>
2025-01-06 11:33:37 +0300

    [app][login]-LoginService

--- crypto/src/main/java/itu/crypto/service/LoginService.java
+++ crypto/src/main/java/itu/crypto/service/LoginService.java
@@ -2,12 +2,9 @@ package itu.crypto.service;
 
 import itu.crypto.CryptoConfigProperties;
 import itu.crypto.dto.ApiResponse;
-import itu.crypto.dto.login.LoginDTO;
+import itu.crypto.dto.login.LoginRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.DefaultResponseErrorHandler;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
@@ -15,32 +12,11 @@ public class LoginService {
     private final FetchService fetchService;
     private final CryptoConfigProperties cryptoConfigProperties;
 
-    public ApiResponse sendLoginDto(LoginDTO loginDTO) {
-	//	return fetchService.fetchUrl("/api/login", loginDTO);
-
-	String laravelUrl = cryptoConfigProperties.getLaravelUrl();
-	String apiUrl = laravelUrl + "/api/login";
-
-	// Faire un appel vers un api
-	RestTemplate restTemplate = new RestTemplate();
-	restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
-	    @Override
-	    public void handleError(ClientHttpResponse response) {
-		// Suppresses default error handling
-	    }
-	});
-
-	try {
-	    String responseBody = restTemplate.postForObject(apiUrl, loginDTO, String.class);
-	    System.out.println(responseBody);
-	    return null;
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    return new ApiResponse("error", "Failed to send login data", null, e.getMessage());
-	}
+    public ApiResponse sendLoginDto(LoginRequest loginRequest) {
+	return fetchService.fetchUrl("/api/login", loginRequest);
     }
 
-    public ApiResponse sendPin(LoginDTO loginDTO) {
-	return fetchService.fetchUrl("/api/login/validate", loginDTO);
+    public ApiResponse sendPin(LoginRequest loginRequest) {
+	return fetchService.fetchUrl("/api/login/validate", loginRequest);
     }
 }
