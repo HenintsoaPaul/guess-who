247cdc845030a77c6aa00aa7158a5d4e111517ee
Nyantemafy <antema.fy01@gmail.com>
2025-01-06 19:34:11 +0200

    test

--- crypto/src/main/java/itu/crypto/repository/TypeMVFundRepository.java
+++ crypto/src/main/java/itu/crypto/repository/TypeMVFundRepository.java
@@ -3,5 +3,5 @@ package itu.crypto.repository;
 import itu.crypto.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TypeMVFundRepository extends JpaRepository<TypeMVFund, Long> {
+public interface TypeMvFundRepository extends JpaRepository<TypeMvFund, Long> {
 }
--- crypto/src/main/java/itu/crypto/service/TransactionService.java
+++ crypto/src/main/java/itu/crypto/service/TransactionService.java
@@ -1,40 +1,45 @@
 package itu.crypto.service;
 
-import java.time.LocalDateTime;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 import itu.crypto.entity.*;
 import itu.crypto.repository.*;
 import java.sql.*;
 
+@Service
 public class TransactionService {
-    @Autowired
+
     private final AccountRepository accountRepository;
+    private final MvFundRepository mvFundRepository;
 
     @Autowired
-    private final MvFundRepository MvFundRepository;
+    public TransactionService(AccountRepository accountRepository, MvFundRepository mvFundRepository) {
+        this.accountRepository = accountRepository;
+        this.mvFundRepository = mvFundRepository;
+    }
 
-    public void Depot(Account account, double montant, Date date, int quantity ) {
+    public void Depot(Account account, double montant, Date date, int quantity) {
         double fund = account.getFund();
 
         TypeMvFund type = new TypeMvFund("Depot");
         MvFund mvFund = new MvFund(date, quantity, type, account);
-        MvFundRepository.save(mvFund);
+        mvFundRepository.save(mvFund);
 
-        account.setFund(account.getFund() + montant);
+        double nvfund = fund + montant;
+        account.setFund(nvfund);
 
         accountRepository.save(account);
     }
 
-    public void Retait(Account account, double montant, Date date, int quantity ) {
+    public void Retait(Account account, double montant, Date date, int quantity) {
         double fund = account.getFund();
 
         TypeMvFund type = new TypeMvFund("Retrait");
         MvFund mvFund = new MvFund(date, quantity, type, account);
-        MvFundRepository.save(mvFund);
+        mvFundRepository.save(mvFund);
 
-        account.setFund(account.getFund() + montant);
+        double nvfund = fund - montant;
+        account.setFund(nvfund);
 
         accountRepository.save(account);
     }
