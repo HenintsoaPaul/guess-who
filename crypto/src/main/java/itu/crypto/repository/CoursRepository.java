d65de99ce7233ee123201d751c16583109f0cf24
Jason Chris <150683198+RdjcMada@users.noreply.github.com>
2025-01-10 10:41:35 +0300

    Chart

--- crypto/src/main/java/itu/crypto/repository/CoursRepository.java
+++ crypto/src/main/java/itu/crypto/repository/CoursRepository.java
@@ -2,6 +2,8 @@ package itu.crypto.repository;
 
 import itu.crypto.entity.Cours;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface CoursRepository extends JpaRepository<Cours, Integer> {
+    List<Cours> findById(int idCrypto);
 }
