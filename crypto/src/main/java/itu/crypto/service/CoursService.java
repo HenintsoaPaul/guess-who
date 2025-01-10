aeb79f93e5620b5fdc89917c39deb383764c3012
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-01-10 08:50:54 +0300

    generer cours pour les crypto

--- crypto/src/main/java/itu/crypto/service/CoursService.java
+++ crypto/src/main/java/itu/crypto/service/CoursService.java
@@ -1,19 +1,41 @@
 package itu.crypto.service;
 
 import itu.crypto.entity.Cours;
+import itu.crypto.entity.Crypto;
 import itu.crypto.repository.CoursRepository;
+import itu.crypto.repository.CryptoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class CoursService {
     private final CoursRepository coursRepository;
+    private final CryptoRepository cryptoRepository;
 
     public List<Cours> findCurrentCours() {
         List<Cours> o = null;
         return o;
+    }
+
+    public List<Cours> generateCours() {
+        List<Crypto> cryptos = cryptoRepository.findAll();
+        for (Crypto crypto : cryptos) {
+            Cours cours = new Cours();
+            cours.setCrypto(crypto);
+            cours.setDateCours(LocalDate.now());
+            cours.setPu(generateRandomPrice());
+            coursRepository.save(cours);
+        }
+        return coursRepository.findAll();
+    }
+
+    private double generateRandomPrice() {
+        Random random = new Random();
+        return 50 + (1000 - 50) * random.nextDouble();
     }
 }
