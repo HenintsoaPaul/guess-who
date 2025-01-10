1e118252629ffb18d3cf129817480d241379d75b
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-01-10 09:43:53 +0300

    find the current cours

---
+++ crypto/src/main/java/itu/crypto/controller/ApiCoursController.java
@@ -0,0 +1,22 @@
+package itu.crypto.controller;
+
+import itu.crypto.entity.Cours;
+import itu.crypto.service.CoursService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cours")
+@RequiredArgsConstructor
+public class ApiCoursController {
+    private final CoursService coursService;
+
+    @GetMapping("/generate")
+    public List<Cours> generateCours() {
+        return coursService.generateCours();
+    }
+}
--- crypto/src/main/java/itu/crypto/service/CoursService.java
+++ crypto/src/main/java/itu/crypto/service/CoursService.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,11 +19,22 @@ public class CoursService {
     private final CoursRepository coursRepository;
     private final CryptoRepository cryptoRepository;
 
+    /**
+     * Retrieves the latest course for each crypto.
+     *
+     * @return a list of the latest courses for each crypto.
+     */
     public List<Cours> findCurrentCours() {
-        List<Cours> o = null;
-        return o;
+        return coursRepository.findLatestCoursForEachCrypto();
     }
 
+    /**
+     * Generates a list of Cours objects for all available Crypto entities.
+     * For each Crypto, a new Cours object is created with the current date and a random price.
+     * The generated Cours objects are then saved to the repository.
+     *
+     * @return a list of all Cours objects saved in the repository.
+     */
     public List<Cours> generateCours() {
         List<Crypto> cryptos = cryptoRepository.findAll();
         for (Crypto crypto : cryptos) {
