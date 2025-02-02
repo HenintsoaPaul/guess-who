1f576bbf4d7409b1a17262bd7d08f94357c687f7
Henintsoa Paul <henintsoapaul@gmail.com>
2025-02-02 21:05:29 +0300

    [cours]-Add CoursDocument for firebase

---
+++ crypto/src/main/java/itu/crypto/firebase/firestore/cours/CoursDocument.java
@@ -0,0 +1,34 @@
+package itu.crypto.firebase.firestore.cours;
+
+import com.google.cloud.Timestamp;
+import itu.crypto.entity.Cours;
+import itu.crypto.entity.Crypto;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+public class CoursDocument {
+    private Integer id;
+    private double pu;
+    private Timestamp dateCours;
+    private Crypto crypto;
+
+    public CoursDocument(Cours cours) {
+        this.id = cours.getId();
+        this.pu = cours.getPu();
+        this.dateCours = Timestamp.of(Date.from(cours.getDateCours().toInstant(ZoneOffset.UTC)));
+        this.crypto = cours.getCrypto();
+    }
+
+    public Cours toEntity() {
+        return new Cours(
+                pu,
+                dateCours.toDate().toInstant().atOffset(ZoneOffset.UTC).toLocalDateTime(),
+                crypto
+        );
+    }
+}
