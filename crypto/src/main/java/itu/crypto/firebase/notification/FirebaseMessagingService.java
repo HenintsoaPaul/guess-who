37465783d30eda310c21f93b4a2c0083d7de642f
Henintsoa Paul <henintsoapaul@gmail.com>
2025-02-05 04:22:07 +0300

    [notification]-Add configuration and service for notification

---
+++ crypto/src/main/java/itu/crypto/firebase/notification/FirebaseMessagingService.java
@@ -0,0 +1,40 @@
+package itu.crypto.firebase.notification;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class FirebaseMessagingService {
+
+    private FirebaseMessaging firebaseMessaging;
+
+    public void sendNotificationByToken(NotificationMessage notificationMessage)
+            throws FirebaseMessagingException {
+        Notification notification = Notification.builder()
+                .setTitle(notificationMessage.getTitle())
+                .setBody(notificationMessage.getBody())
+                .setImage(notificationMessage.getImage())
+                .build();
+
+        Message message = Message.builder()
+                .setToken(notificationMessage.getRecipientToken())
+                .setNotification(notification)
+                .putAllData(notificationMessage.getData())
+                .build();
+
+        try {
+            firebaseMessaging.send(message);
+            log.info("Notification sent successfully");
+        } catch (FirebaseMessagingException e) {
+            log.error("Error sending notification. Message: {}", e.getMessage());
+            throw e;
+        }
+    }
+}
