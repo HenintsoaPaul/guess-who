244f119d486360b356d2983de2d4c00a224116bf
Henintsoa Paul <henintsoapaul@gmail.com>
2025-02-09 15:04:17 +0300

    [sale]-Exception

--- crypto/src/main/java/itu/crypto/entity/sale/SaleException.java
+++ crypto/src/main/java/itu/crypto/entity/sale/SaleException.java
@@ -3,7 +3,7 @@ package itu.crypto.entity.sale;
 import itu.crypto.entity.wallet.Wallet;
 
 public class SaleException extends Exception {
-    private static final String NOT_ENOUGH_CRYPTO_ERROR = "Not enough cryptos in wallet. Crypto: %s, available: %d, required: %d";
+    private static final String NOT_ENOUGH_CRYPTO_ERROR = "Pas assez de cryptos dans votre portefeuille: [Crypto: %s, available: %d, required: %d]";
 
     public SaleException(String message) {
         super(message);
