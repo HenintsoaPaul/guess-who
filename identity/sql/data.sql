7b413bee533e5f4e7c77303997a790341e3c071b
Nyantemafy <antema.fy01@gmail.com>
2025-02-09 20:32:55 +0200

    ok swagger

--- identity/sql/data.sql
+++ identity/sql/data.sql
@@ -15,13 +15,13 @@ VALUES ('alice@example.com', 'mypassword', NOW(), NOW() + INTERVAL '30 days', NO
        ('jack@example.com', 'mypassword', NOW(), NOW() + INTERVAL '30 days', NOW(), '1234');
 
 INSERT INTO account (email, password, attempt, max_attempt, id_pending_register, id_type_account_state)
-VALUES ('Alice', 'alice@example.com', 'mypassword', 3, 0, 1, 10000),
-       ('Bob', 'bob@example.com', 'mypassword', 3, 0, 2, 12000),
-       ('Charlie', 'charlie@example.com', 'mypassword', 3, 0, 3, 15000),
-       ('Diana', 'diana@example.com', 'mypassword', 3, 0, 4, 8000),
-       ('Eve', 'eve@example.com', 'mypassword', 3, 0, 5, 9000),
-       ('Frank', 'frank@example.com', 'mypassword', 3, 0, 6, 7000),
-       ('Grace', 'grace@example.com', 'mypassword', 3, 0, 7, 9500),
-       ('Hank', 'hank@example.com', 'mypassword', 3, 0, 8, 11000),
-       ('Ivy', 'ivy@example.com', 'mypassword', 3, 0, 9, 10500),
-       ('Jack', 'jack@example.com', 'mypassword', 3, 0, 10, 11500);
+VALUES ('alice@example.com', 'mypassword', 0, 3, 1, 1),
+       ('bob@example.com', 'mypassword', 1, 3, 2, 1),
+       ('charlie@example.com', 'mypassword', 3, 0, 3, 1),
+       ('diana@example.com', 'mypassword', 0, 3, 4, 2),
+       ('eve@example.com', 'mypassword', 0, 3, 5, 2),
+       ('frank@example.com', 'mypassword', 0, 3, 6, 1),
+       ('grace@example.com', 'mypassword', 0, 3, 7, 1),
+       ('hank@example.com', 'mypassword', 1, 3, 8, 1),
+       ('ivy@example.com', 'mypassword', 1, 3, 9, 1),
+       ('jack@example.com', 'mypassword', 1, 3, 10, 1);
